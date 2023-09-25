cc57e0c935770ea28d6e290a84717065ef4de26a
mickmao <141818035+mickmao@users.noreply.github.com>
2023-09-24 22:21:37 -0500

    BEMBlock class

--- src/cli.ts
+++ src/cli.ts
@@ -1,27 +1,27 @@
-import meow from 'meow'
-import { parse } from './api.js'
+// import meow from 'meow'
+// import { parse } from './api.js'
 
-const cli = meow(
-	`
-	Usage
-		$ bem <input>
-	
-	Options
-		--rainbow, -r  Include a rainbow
-	
-	Examples
-		$ bem unicorns --rainbow
-		🌈 unicorns 🌈
-`,
-	{
-		importMeta: import.meta,
-		flags: {
-			rainbow: {
-				type: 'boolean',
-				shortFlag: 'r'
-			}
-		}
-	}
-)
+// const cli = meow(
+// 	`
+// 	Usage
+// 		$ bem <input>
 
-parse(cli.input.at(0) ?? '', cli.flags)
+// 	Options
+// 		--rainbow, -r  Include a rainbow
+
+// 	Examples
+// 		$ bem unicorns --rainbow
+// 		🌈 unicorns 🌈
+// `,
+// 	{
+// 		importMeta: import.meta,
+// 		flags: {
+// 			rainbow: {
+// 				type: 'boolean',
+// 				shortFlag: 'r'
+// 			}
+// 		}
+// 	}
+// )
+
+// parse(cli.input.at(0) ?? '', cli.flags)
